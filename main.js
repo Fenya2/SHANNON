fc2238fd56cc64a61571a2b598273bfb779de097
fenya00 <dim74.09@mail.ru>
2022-09-25 17:01:41 +0500

    fixed output

--- main.js
+++ main.js
@@ -23,7 +23,10 @@ try {
     let inputSequence = fs.readFileSync(fileName).toString(); // get sequence
     console.log(inputSequence);
     let series = makeSeries(inputSequence);
-    if(series.length === 0 || series.length === 1) console.log(`entropy is: 0`);
+    if(series.length === 0 || series.length === 1) {
+        console.log(`entropy is: 0`);
+        throw Error('sum is 0 or 1');
+    }
     let sum = 0;
     let p = 0;
     for(let i = 0; i < series.length; i++) {
@@ -33,6 +36,8 @@ try {
     sum /= Math.log(2);
     console.log(`entropy is: ${sum}`);
 } catch (e) {
-    showHelp();
-    console.error(e.message);
+    if(!(e.message === 'sum is 0 or 1')) {
+        showHelp();
+        console.error(e.message);
+    }
 }
